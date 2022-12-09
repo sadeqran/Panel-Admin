47de94cd29fbadd7e122b7c89c9f9130e364416b
sadeqran <sadiq.ranjbar@gmail.com>
2022-12-09 12:27:48 +0330

    18azar1

--- src/layouts/admin/navbar/RightContent.jsx
+++ src/layouts/admin/navbar/RightContent.jsx
@@ -6,7 +6,7 @@ const Rightcontent = () => {
   return (
     <div className="right_content h-100 py-1 bg-dark">
       <a className="navbar-brand h-100" href="/">
-        <img src="/assets/images/logo.png" className="h-100" alt="/"/>
+        {/* <img src="/assets/images/logo.png" className="h-100" alt="/"/> */}
       </a>
       <div className="form-check form-switch mx-4 d-none d-md-block">
         <input
